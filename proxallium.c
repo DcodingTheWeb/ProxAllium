72cabcb29cc9f2fbde432452a236ba70b718be01
Damon Harris (TheDcoder) <TheDcoder@protonmail.com>
2019-01-09 20:14:18 +0530

    Add options and support for supplying Tor config

--- proxallium.c
+++ proxallium.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "allium/allium.h"
+
+struct {
+	int port;
+	bool localhost_only;
+} options = {
+	9051,
+	true
+};
+
+char *proxallium_gen_torrc();
 
 int main(void) {
 	// Create a new instance of Tor
@@ -10,10 +21,15 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 	
+	// Generate the tor config according to the options
+	char *config = proxallium_gen_torrc();
+	if (!config) {
+		puts("Failed to generate configuration for Tor!");
+		return EXIT_FAILURE;
+	}
+	
 	// Start Tor
-	if (!
-	allium_start(tor_instance)
-	) {
+	if (!allium_start(tor_instance, config)) {
 		puts("Failed to start Tor!");
 		return EXIT_FAILURE;
 	}
@@ -22,4 +38,46 @@ int main(void) {
 	// Clean up and exit
 	free(tor_instance);
 	return EXIT_SUCCESS;
+}
+
+char *proxallium_gen_torrc() {
+	// Temp variables to store format strings and their sizes
+	char *format_string;
+	int string_size;
+	
+	// Socks Port
+	format_string = "SocksPort %i\n";
+	string_size = snprintf(NULL, 0, format_string, options.port);
+	char socks_port[string_size + 1];
+	snprintf(socks_port, string_size + 1, format_string, options.port);
+	
+	// Only allow connections from localhost explicitly for safety
+	char *localhost_explicit = options.localhost_only ?
+		"SOCKSPolicy accept 127.0.0.1\n"
+		"SOCKSPolicy accept6 [::1]\n"
+		"SOCKSPolicy reject *\n"
+	: "";
+	
+	// Generate the final string with all of the configuration
+	char *config_sections[] = {
+		socks_port,
+		localhost_explicit,
+		NULL
+	};
+	
+	// Calculate the total required size for the string
+	size_t total_size = 1;
+	for (char **section = config_sections; *section != NULL; ++section) {
+		total_size += strlen(*section);
+	}
+	
+	// Allocate space for the string and populate it
+	char *torrc = malloc(total_size);
+	if (!torrc) return NULL;
+	torrc[0] = '\0';
+	for (char **section = config_sections; *section != NULL; ++section) {
+		strcat(torrc, *section);
+	}
+	
+	return torrc;
 }
