d8bbcc93a9a1a2ad2e5b95055fd23863ac6c8324
Damon Harris <TheDcoder@protonmail.com>
2020-05-05 18:46:47 +0530

    Improve log functions

--- log.c
+++ log.c
@@ -4,20 +4,44 @@
 
 void log_output(const char *format, ...) {
 	va_list args;
+	
 	va_start(args, format);
-	log_output_raw_var(format, args);
+	int buffer_size = log_calc_buf_size(format, args);
 	va_end(args);
-	log_output_raw("\n");
+	
+	va_start(args, format);
+	log_output_raw_var(format, buffer_size, args);
+	va_end(args);
+	
+	log_output_text("\n");
 }
 
-int log_output_raw(const char *format, ...) {
+void log_output_raw(const char *format, ...) {
 	va_list args;
+	
 	va_start(args, format);
-	int bytes_written = log_output_raw_var(format, args);
+	int buffer_size = log_calc_buf_size(format, args);
 	va_end(args);
-	return bytes_written;
+	
+	va_start(args, format);
+	log_output_raw_var(format, buffer_size, args);
+	va_end(args);
 }
 
-int log_output_raw_var(const char *format, va_list args) {
-	return vprintf(format, args);
+int log_calc_buf_size(const char *format, va_list args) {
+	// Calculate required size to store the formatted string
+	return vsnprintf(NULL, 0, format, args) + 1;
+}
+
+void log_output_raw_var(const char *format, int buffer_size, va_list args) {
+	// Store the formatted string
+	char string[buffer_size];
+	vsnprintf(string, buffer_size, format, args);
+	
+	// Pass the string for output
+	log_output_text(string);
+}
+
+void log_output_text(const char *string) {
+	fputs(string, stdout);
 }
--- log.h
+++ log.h
@@ -1,8 +1,12 @@
+#include <stdarg.h>
+
 #ifndef LOG_H
 #define LOG_H
 
 void log_output(const char *format, ...);
-int log_output_raw(const char *format, ...);
-int log_output_raw_var(const char *format, va_list args);
+void log_output_raw(const char *format, ...);
+int log_calc_buf_size(const char *format, va_list args);
+void log_output_raw_var(const char *format, int buffer_size, va_list args);
+void log_output_text(const char *string);
 
 #endif
